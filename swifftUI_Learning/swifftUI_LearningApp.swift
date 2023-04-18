74b870ceb12f1ccaf6ac94b60acecf9e8df5c87b
Mohammad Subkhan <subkhanpinter22@gmail.com>
2023-04-18 16:12:08 +0700

    Initial Commit

---
+++ swifftUI_Learning/swifftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifftUI_LearningApp.swift
+//  swifftUI_Learning
+//
+//  Created by masbek mbp-m2 on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct swifftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
